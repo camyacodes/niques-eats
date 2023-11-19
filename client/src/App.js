4238048d8f538515328802655369ca66ffe6b717
Mya <87587644+camyacodes@users.noreply.github.com>
2023-11-18 20:52:24 -0500

    added pages for orders

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 // CONNECT FRONT END TO BACK END USING APOLLO CLIENT
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // COMPONENTS
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 // PAGES
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import Menu from "./pages/Menu";
+import Checkout from "./pages/Checkout";
+import OrderHistory from "./pages/OrderHistory";
+import NoMatch from "./pages/NoMatch";
 
 const client = new ApolloClient({
   uri: "/graphql",
@@ -15,15 +22,24 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div className="container">
-          <Menu />
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/menu" element={<Menu />} />
+              <Route path="/checkout" element={<Checkout />} />
+              <Route path="/orderhistory" element={<OrderHistory />} />
+              <Route path="*" element={<NoMatch />} />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
-
 export default App;
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <h1>Deep Thoughts</h1>
+        <Link to="/">
+          <h1>Nique's Eats</h1>
+        </Link>
+
+        <nav className="text-center">
+          <Link to="/Menu">Menu</Link>
+          <Link to="/Login">Login</Link>
+        </nav>
       </div>
     </header>
   );
