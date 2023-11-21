05361ebd65563304597b38c4bb3b3355685b73e7
Mya <87587644+camyacodes@users.noreply.github.com>
2023-11-20 22:58:18 -0500

    cart is fully functional

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -20,7 +20,28 @@ import moment from "moment";
 function ProductItem(item) {
   const { image, name, _id, price, description } = item;
 
-  // const [state, dispatch] = useStoreContext();
+  const [state, dispatch] = useStoreContext();
+
+  const { cart } = state;
+
+  const addToCart = () => {
+    // find the cart item with the matching id
+    const itemInCart = cart.find((cartItem) => cartItem._id === _id);
+
+    // if there was a match, call UPDATE with a new purchase quantity
+    if (itemInCart) {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: _id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+      });
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        product: { ...item, purchaseQuantity: 1 },
+      });
+    }
+  };
   // const [deliveryDate, setDeliveryDate] = useState(null);
 
   // const { cart } = state;
@@ -73,45 +94,118 @@ function ProductItem(item) {
   //   return <h3>Check back later for our update menu!</h3>;
   // }
   return (
-    <Row>
-      <Container
-        className="themed-container"
-        fluid={true}
-        id="dish-card"
-        key={_id}
+    <div>
+      <Row>
+        <Container
+          className="themed-container"
+          fluid={true}
+          id="dish-card"
+          key={_id}
+        >
+          <Col key={_id}>
+            <Card id="dish-card-content">
+              <div>
+                <CardImg
+                  top
+                  width="100%"
+                  src={`/images/${image}`}
+                  alt={name}
+                  id="dish-img"
+                  className="rounded mx-auto d-block"
+                />
+              </div>
+
+              <CardBody>
+                <div className="row">
+                  <CardTitle tag="h5" className="col " id="card-text">
+                    {name}
+                  </CardTitle>
+                  <CardSubtitle
+                    tag="h5"
+                    className="col text-end"
+                    id="card-text"
+                  >
+                    ${price}
+                  </CardSubtitle>
+                </div>
+                <div id="card-btn">
+                  <button
+                    type="button"
+                    className="btn btn-primary add-to-cart"
+                    data-bs-toggle="modal"
+                    data-bs-target={`#modal${_id}`}
+                    // onClick={addToCart}
+                  >
+                    Add to Cart
+                  </button>
+                </div>
+              </CardBody>
+            </Card>
+          </Col>
+        </Container>
+      </Row>
+
+      <div
+        className="modal fade"
+        id={`modal${_id}`}
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
       >
-        <Col key={_id}>
-          <Card id="dish-card-content">
-            <div>
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+
+            <div className="modal-body row">
               <CardImg
                 top
                 width="100%"
                 src={`/images/${image}`}
                 alt={name}
-                id="dish-img"
-                className="rounded mx-auto d-block"
+                id="dish-img-nav"
+                className="rounded mx-auto d-block col"
               />
-            </div>
-
-            <CardBody>
-              <div className="row">
-                <CardTitle tag="h5" className="col " id="card-text">
+              <h6 className="col" id="description">
+                {description}
+              </h6>
+              <div className="row align-items-center">
+                <CardTitle tag="h5" className="col text-start" id="name">
                   {name}
                 </CardTitle>
-                <CardSubtitle tag="h5" className="col text-end" id="card-text">
+                <CardSubtitle tag="h5" className="col text-end" id="price">
                   ${price}
                 </CardSubtitle>
               </div>
-              <div id="card-btn">
-                <button type="button" className="btn btn-primary add-to-cart">
-                  Add to Cart
-                </button>
-              </div>
-            </CardBody>
-          </Card>
-        </Col>
-      </Container>
-    </Row>
+            </div>
+
+            <div className="modal-footer">
+              <Datetime
+                value={"Choose a Date"}
+                timeFormat={false}
+                // isValidDate={valid}
+                // onChange={onDateChange}
+              />
+              <button
+                type="button"
+                className="add-to-cart"
+                id="modal-btn"
+                data-bs-dismiss="modal"
+                onClick={addToCart}
+              >
+                Add to Cart
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
